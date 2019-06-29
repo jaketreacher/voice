13765b6662ae2e21e2e1c75e23fa1d1783342b0c
Jake Treacher <git@jaketreacher.com>
2019-06-29 17:31:57 +0800

    Add Team views

--- web/urls.py
+++ web/urls.py
@@ -5,6 +5,10 @@ from . import views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='web/home.html'), name='home'),
+
     path('candidates/', views.CandidateListView.as_view(), name='candidates'),
     path('candidate/<int:pk>', views.CandidateDetailView.as_view(), name='candidate-detail'),
+
+    path('teams/', views.TeamListView.as_view(), name='teams'),
+    path('team/<int:pk>', views.TeamDetailView.as_view(), name='team-detail'),
 ]
--- web/views.py
+++ web/views.py
@@ -3,7 +3,8 @@ from django.utils.decorators import method_decorator
 from django.views import generic
 
 from .models import (
-    Candidate
+    Candidate,
+    Team
 )
 
 
@@ -23,3 +24,21 @@ class CandidateListView(generic.ListView):
 @method_decorator(login_required, name='dispatch')
 class CandidateDetailView(generic.DetailView):
     model = Candidate
+
+
+@method_decorator(login_required, name='dispatch')
+class TeamListView(generic.ListView):
+    context_object_name = 'team_list'
+    template_name = 'team_list.html'
+
+    def get_queryset(self):
+        if self.request.user.is_mentor:
+            return Team.objects.filter(mentors__user=self.request.user)
+
+        if self.request.user.is_admin:
+            return Team.objects.all()
+
+
+@method_decorator(login_required, name='dispatch')
+class TeamDetailView(generic.DetailView):
+    model = Team
