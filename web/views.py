e9d866b8743f66db41994b29d16481b16f8ef1d4
Jake Treacher <git@jaketreacher.com>
2019-06-29 16:32:54 +0800

    Add auth to Candidate views

--- web/views.py
+++ web/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views import generic
 
 from .models import (
@@ -5,9 +7,19 @@ from .models import (
 )
 
 
+@method_decorator(login_required, name='dispatch')
 class CandidateListView(generic.ListView):
-    model = Candidate
+    context_object_name = 'candidate_list'
+    template_name = 'candidate_list.html'
+
+    def get_queryset(self):
+        if self.request.user.is_mentor:
+            return Candidate.objects.filter(team__mentors__user=self.request.user)
+
+        if self.request.user.is_admin:
+            return Candidate.objects.all()
 
 
+@method_decorator(login_required, name='dispatch')
 class CandidateDetailView(generic.DetailView):
     model = Candidate
