877d0b665346c52a210da3252b68de24b105c0d1
Jake Treacher <git@jaketreacher.com>
2019-07-01 16:17:19 +0800

    Refactor login url and template

--- web/urls.py
+++ web/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
 
 from . import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='web/home.html'), name='home'),
+    path('', auth_views.LoginView.as_view(template_name='web/home.html'), name='home'),
+    path('accounts/login/rediect/', views.login_redirect, name='login-redirect'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     path('candidates/', login_required(views.CandidateListView.as_view()), name='candidates'),
     path('candidate/<int:pk>', login_required(views.CandidateDetailView.as_view()), name='candidate-detail'),
@@ -17,8 +18,4 @@ urlpatterns = [
     path('activities/', login_required(views.ActivityListView.as_view()), name='activities'),
 
     path('activity/<int:pk>/score', login_required(views.ActivityScoreFormView.as_view()), name='activity-form'),
-
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/login/rediect/', views.login_redirect, name='login-redirect'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
